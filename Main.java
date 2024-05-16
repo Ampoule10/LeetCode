7d8ccf57c4ea67b1192c4457bbba6571f9920909
Ampoule10 <ampoule10@gmail.com>
2024-05-16 09:04:10 +0200

    28. Find the Index of the First Occurrence in a String

--- Main.java
+++ Main.java
@@ -1,30 +1,19 @@
-import java.util.*;
-
 public class Main {
     public static void main(String[] args) {
+        String haystack = "saqbutsad";
+        String needle = "sad";
+        System.out.println(getIndex(haystack, needle));
+    }
 
-        List<Integer> listFirst = new ArrayList<>();
-        listFirst.add(1);
-        listFirst.add(2);
-        listFirst.add(4);
-
-        List<Integer> listSecond = Arrays.asList(1, 3, 4);
-
-        for(int i = 0; i<listSecond.size(); i++){
-            listFirst.add(listSecond.get(i));
-        }
-
-        for (int j = 0; j < listFirst.size(); j++) {
-            for (int i = 0; i < listFirst.size() - 1; i++) {
-                if (listFirst.get(i) > listFirst.get(i + 1)) {
-                    int temp = listFirst.get(i);
-                    listFirst.set(i, listFirst.get(i + 1));
-                    listFirst.set((i + 1), temp);
+    private static int getIndex(String haystack, String needle) {
+        for (int i = 0; i < haystack.length() - needle.length() + 1; i++) {
+            if (haystack.charAt(i) == needle.charAt(0)) {
+                String checkNeedle = haystack.substring(i, i + needle.length());
+                if (checkNeedle.equals(needle)) {
+                    return i;
                 }
             }
         }
-
-        System.out.println(listFirst);
-
+        return -1;
     }
 }
